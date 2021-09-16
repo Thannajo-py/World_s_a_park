50bc4116bd6440f794238269e0af04f0710f4b71
Thannajo-py <wolgan_4@hotmail.com>
2021-09-16 11:06:10 +0200

    end cleaning

--- HTML_CSS/objection.js
+++
@@ -1,6 +0,0 @@
-const audio = new Audio("phoenix-objection.mp3");
-const objection = document.getElementById("objection")
-objection.addEventListener("click",()=>{
-    console.log("ok")
-    audio.play()
-})
