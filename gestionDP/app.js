4f11ba164523c3aaec33d54f8cd747c6313b1d29
Thannajo-py <wolgan_4@hotmail.com>
2021-09-16 12:06:34 +0200

    maj bdd

--- gestionDP/app.js
+++ gestionDP/app.js
@@ -41,9 +41,10 @@ app.use(express.static(path.join(__dirname, '/public')))
         const id = req.params.id
         let query = `SELECT * FROM parks WHERE id = ${id};`;
         connection.query(query, (err, result) => {
-            if (err) throw err; 
-            console.log(result)
-            res.render("parcs.ejs",{park:result[0]})
+            if (err || result[0] == undefined){
+                res.sendFile('../public/main.html');
+            }
+            else{res.render("parcs.ejs",{park:result[0]})}
             
 
     })
